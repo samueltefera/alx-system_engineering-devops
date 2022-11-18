4c5d6dce38548e9941f9c20dbcb38933c1223ebf
Samuel Tefera <samueltefer23@gmail.com>
2022-11-18 13:29:30 +0000

    check pylint

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,27 +3,28 @@
     Takes an argument passed to the script to be used in an API query.
     Retrieved data is then parsed to be dumped in a CSV file.
 """
+import sys
 import csv
 import json
 import requests
-import sys
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         user_d = {}
         todo_l = []
-        url = 'https://jsonplaceholder.typicode.com/'
-        user_resp = requests.get(url + 'users/{}'.format(sys.argv[1]))
-        todo_resp = requests.get(url + 'todos?userId={}'.format(sys.argv[1]))
+        URL = 'https://jsonplaceholder.typicode.com/'
+        user_resp = requests.get(URL + f'users/{sys.argv[1]}', timeout=3)
+        todo_resp = requests.get(URL + f'todos?userId={sys.argv[1]}', timeout=3)
         if all([user_resp, todo_resp]) is not None:
             try:
                 user_d = json.loads(user_resp.text)
                 todo_l = json.loads(todo_resp.text)
-            except JSONDecodeError:
+            except json.JSONDecodeError:
                 pass
-            if type(todo_l) is list and type(user_d) is dict and user_d != {}:
-                with open(sys.argv[1] + '.csv', 'w', newline='') as csvfile:
+            if isinstance(todo_l, list) and isinstance(user_d, dict) and user_d != {}:
+                with open(sys.argv[1] + '.csv', 'w', newline='', encoding="utf-8") as csvfile:
                     write_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
                     for todo in todo_l:
                         line = [user_d.get('id'), user_d.get('username'),
